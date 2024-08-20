e21c6fff67c9d9e29c25b570c36f9989144809f8
bidalalsrb <bidal.mingyu@gmail.com>
2024-08-20 19:36:21 +0900

    List filter, toLowerCase 추가

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Editor onCreate={onCreate}></Editor>
-      <List></List>
+      <List todos={todos}></List>
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import './List.css';
 import TodoItem from './TodoItem';
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState('');
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getFilterdData = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+  const filterdTodos = getFilterdData();
   return (
     <div className="List">
       <h4>Todo List☘️</h4>
-      <input placeholder="검색어를 입력하세요"></input>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      ></input>
       <div className="todos_wrapper">
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
+        {filterdTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
